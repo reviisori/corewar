7f8c33a8089c89b0107dfcdfe87370fe23afb6d4
Aleksandr Tikka <altikka@student.hive.fi>
2022-10-26 17:02:54 +0300

    added call to read_source()

--- asm/src/parse.c
+++ asm/src/parse.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 15:16:15 by altikka           #+#    #+#             */
-/*   Updated: 2022/10/26 16:15:42 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/26 16:47:55 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int	parse(char *filename)
 	t_src	s;
 
 	init_source(&s);
-	(void)filename;
+	if (read_source(&s, filename) < 0)
+		exit(0); //msg
 	ft_printf("parsing done - not.\n");
 	return (1);
 }
