72ea2df1626b81614dec495d5bc7e648f9a21472
home <aleksandr.tikka@gmail.com>
2022-10-26 23:07:21 +0300

    added calls to panic()

--- asm/src/main.c
+++ asm/src/main.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 14:29:02 by altikka           #+#    #+#             */
-/*   Updated: 2022/10/26 15:38:28 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/26 21:46:13 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 int	main(int argc, char **argv)
 {
 	if (!valid_file(argc, argv[1]))
-		return (ft_putstr(ASM_USAGE), -1);
+		return (panic(ASM_USAGE));
 	if (!parse(argv[1]))
-		ft_putendl("parsing error");
+		return (panic("Parsing failed."));
 	if (!assemble(argv[1]))
-		ft_putendl("avengers didn't assemble :(");
+		return (panic("Avengers didn't assemble."));
 	return (0);
 }
