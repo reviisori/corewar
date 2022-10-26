0d0e93647c04d3c4e293546a3ac32f7ac40395f2
home <aleksandr.tikka@gmail.com>
2022-10-26 23:15:57 +0300

    WIP

--- asm/src/read_source.c
+++ asm/src/read_source.c
@@ -6,28 +6,42 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 16:44:38 by altikka           #+#    #+#             */
-/*   Updated: 2022/10/26 17:01:48 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/26 23:15:06 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-static int	get_buf_size(int fd, t_src *s)
+static off_t	get_buf_size(int fd)
 {
+	off_t	start;
+	off_t	size;
+
+	start = lseek(fd, 0, SEEK_CUR);
+	size = lseek(fd, 0, SEEK_END);
+	if (start == -1 || size == -1 || start == size)
+		panic_source(fd, "Couldn't get buffer size.");
+	return (size);
 }
 
 static int	read_to_buf(int fd, t_src *s)
 {
+	size_t	buf_size;
+
+	buf_size = (size_t) get_buf_size(fd);
+	(void)s; //here ft_vecresize()
+	return (1);
 }
 
 int	read_source(t_src *s, const char *filename)
 {
 	int	fd;
 
-	fd = open(filename, 0_RDONLY);
-	if (fd < 0)
-		return (ft_putendl("Error: open."), exit(0));
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (panic("Opening file failed."));
 	if (read_to_buf(fd, s) < 0)
-		return (ft_putendl("Error: GBS."), exit(0));
+		return (panic("Reading file failed."));
+	close(fd);
 	return (1);
 }
