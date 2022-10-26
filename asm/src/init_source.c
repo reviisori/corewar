e19cef8bff4736e8ae4d8a376de8c7def6b83660
home <aleksandr.tikka@gmail.com>
2022-10-26 23:06:37 +0300

    changed function's return value, added call to panic()

--- asm/src/init_source.c
+++ asm/src/init_source.c
@@ -6,19 +6,20 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 16:09:27 by altikka           #+#    #+#             */
-/*   Updated: 2022/10/26 16:28:48 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/26 21:56:38 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-void	init_source(t_src *s)
+int	init_source(t_src *s)
 {
 	if (ft_vecnew(&s->buf, 1, sizeof(char)) < 0)
-		return(ft_putendl("Error: Couldn't init source buffer."), exit(0));
+		return (panic("Couldn't initialize source buffer."));
 	s->row = 0;
 	s->col = 0;
 	s->index = 0;
 	s->prev = NULL;
 	s->next = NULL;
+	return (1);
 }
