cae775a719eb313c9981e82ccf26fb6acf715ebf
Aleksandr Tikka <altikka@student.hive.fi>
2022-10-26 16:30:23 +0300

    included source.h and prototyped main functions

--- asm/includes/asm.h
+++ asm/includes/asm.h
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 14:30:34 by altikka           #+#    #+#             */
-/*   Updated: 2022/10/21 14:36:37 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/26 16:18:09 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,16 @@
 # define ASM_H
 
 # include "libft.h"
+# include "source.h"
+//# include "handler.h"
 
-//code stuff
+# define ASM_USAGE "\nusage: ./asm [-WIP] <file.s>\n\
+ options:\n\
+ \t\e[1m-h\e[m\thelp - NOT IMPLEMENTED\n\
+ \t\e[1m-q\e[m\tquiet mode - NOT IMPLEMENTED\n\n"
+
+int		valid_file(int argc, char *filename);
+int		parse(char *filename);
+int		assemble(char *filename);
 
 #endif
