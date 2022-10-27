668d3890e4efb9516e6d46cee577f023bf7e79af
home <aleksandr.tikka@gmail.com>
2022-10-27 09:32:48 +0300

    changed panic_source() functions's return value to void

--- asm/src/cleanup_source.c
+++ asm/src/cleanup_source.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 22:16:26 by altikka           #+#    #+#             */
-/*   Updated: 2022/10/26 22:50:33 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/27 09:14:26 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	free_source(t_src *s)
 	s->next = NULL;
 }
 
-int	panic_source(int fd, const char *msg)
+void	panic_source(int fd, const char *msg)
 {
 	ft_putstr_fd("Error: ", 2);
 	ft_putendl_fd(msg, 2);
