7146297c6819fa9226c96f062635687eaee6e79a
home <aleksandr.tikka@gmail.com>
2022-10-27 20:21:43 +0300

    norm

--- asm/includes/symtypes.h
+++ asm/includes/symtypes.h
@@ -6,7 +6,7 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 17:07:22 by atenhune          #+#    #+#             */
-/*   Updated: 2022/10/27 17:09:22 by atenhune         ###   ########.fr       */
+/*   Updated: 2022/10/27 20:16:36 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@
 typedef enum e_symtypes
 {
 	la_unknown,
-
+	la_eof,
+	la_eol
 }	t_symtypes;
 
 #endif
