9376a29c47cc5c7be54bf6d68bf8a26c38f06bc4
Aleksandr Tikka <altikka@student.hive.fi>
2022-10-28 15:33:10 +0300

    renaming

--- asm/src/parse.c
+++ asm/src/parse.c
@@ -6,19 +6,22 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 15:16:15 by altikka           #+#    #+#             */
-/*   Updated: 2022/10/27 09:26:13 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/28 14:04:10 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-int	parse(char *filename)
+int	parse(t_sh *d, char *filename)
 {
 	t_src	s;
 
+	init_handler(d);
 	init_source(&s);
 	if (read_source(&s, filename) < 0)
 		panic("Invalid source file.");
+	//if (parse_header(d, &s) < 0)
+	//	panic("Invalid champion information.");
 	ft_printf("Parsing done - not.\n"); //...
 	return (1);
 }
