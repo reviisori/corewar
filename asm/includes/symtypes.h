c8c2e2d7b1225ab59129f24c3a3730485e853fd0
atenhune <anttipekka.tenhunen@gmail.com>
2022-10-28 18:55:02 +0300

    token struct now has next pointer

--- asm/includes/symtypes.h
+++ asm/includes/symtypes.h
@@ -6,7 +6,7 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 17:07:22 by atenhune          #+#    #+#             */
-/*   Updated: 2022/10/28 15:14:43 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/28 18:53:56 by atenhune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,30 @@
 typedef enum e_symtypes
 {
 	la_unknown,
-	la_eof,
-	la_eol,
-	la_label,
-	la_op,
-	la_comm,
+	la_eof, //
+	la_eol, //
+	la_label, //
+	la_op, //
+	la_comm, // ??
 	la_champname, //
 	la_champcomm, //
-	la_reg,
-	la_dir,
-	la_ind,
+	la_reg, //
+	la_dir, //
+	la_ind, //
 	la_num,
-	la_comma,
+	la_comma, //??
 	la_minus,
 	la_plus
 }	t_symtypes;
 
 typedef struct s_token
 {
-	t_symtypes	symbol;
-	t_vec		*str;
-	bool		is_label;
-	bool		declared;
-	int			num;
-}				t_token;
+	t_symtypes		symbol;
+	t_vec			content;
+	bool			declared;
+	bool			is_label;
+	int				num;
+	struct t_token	*next;
+}					t_token;
 
 #endif
