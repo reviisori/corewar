acf6a085d4ab33419550a54764836f44500f12a5
Aleksandr Tikka <altikka@student.hive.fi>
2022-10-28 11:51:42 +0300

    more symbol types and a plaveholder struct for tokens

--- asm/includes/symtypes.h
+++ asm/includes/symtypes.h
@@ -6,7 +6,7 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 17:07:22 by atenhune          #+#    #+#             */
-/*   Updated: 2022/10/27 20:16:36 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/28 11:40:52 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,28 @@ typedef enum e_symtypes
 {
 	la_unknown,
 	la_eof,
-	la_eol
+	la_eol,
+	la_label,
+	la_op,
+	la_comm,
+	la_champname,
+	la_champcomm,
+	la_reg,
+	la_dir,
+	la_ind,
+	la_num,
+	la_comma,
+	la_minus,
+	la_plus
 }	t_symtypes;
+
+typedef struct s_token
+{
+	t_symtypes	symbol;
+	t_vec		*str;
+	bool		is_label;
+	bool		declared;
+	int			num;
+}				t_token;
 
 #endif
