5a1078fe395fdffa9c1cb93f9570f75b228b7cf8
Aleksandr Tikka <altikka@student.hive.fi>
2022-10-28 15:32:39 +0300

    OMEGAWIP - brb

---
+++ asm/src/parse_header.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_header.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/28 14:04:43 by altikka           #+#    #+#             */
+/*   Updated: 2022/10/28 14:59:06 by altikka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "asm.h"
+/*
+static void	get_name(t_sh *a, t_src *s)
+{
+	bool	skip_all;
+
+	skip_all = false;
+	while ()
+	{
+		s
+	}
+}
+*/
+int		parse_header(t_sh *d, t_src *s)
+{
+//	while (a->token != la_label)
+//	i//get_name(a, s);
+	(void)d;
+	(void)s;
+	return (1);
+}
