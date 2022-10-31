3250d0e4672d66ba184d5cd2873a223d7e5b5340
atenhune <anttipekka.tenhunen@gmail.com>
2022-10-31 15:09:23 +0200

    Hub for different token functions

---
+++ asm/src/lex_tokenization.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lex_tokenization.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/31 11:11:12 by atenhune          #+#    #+#             */
+/*   Updated: 2022/10/31 14:31:53 by atenhune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "asm.h"
+
+void	lex_tokenization(t_src *s, t_token *t)
+{
+	char *target;
+
+	target = (char *)&s->buf.data[s->index];
+	if (*target == '\n' || *target == '\0')
+		lex_endof(s, t);
+	else if (*target == '#' || *target == ';')
+		lex_comment(s, t);
+}
