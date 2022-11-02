7f489cd598c0687e9f5b0caa509aa8c0e75ec137
atenhune <anttipekka.tenhunen@gmail.com>
2022-11-02 15:41:38 +0200

    lex_header() and panic_lex() prototypes added

--- asm/includes/lex.h
+++ asm/includes/lex.h
@@ -6,7 +6,7 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 16:33:03 by atenhune          #+#    #+#             */
-/*   Updated: 2022/10/31 14:11:50 by atenhune         ###   ########.fr       */
+/*   Updated: 2022/11/02 15:38:50 by atenhune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 # define LEX_H
 
 void	init_token(t_token *t);
+void	panic_lex(const char *msg, size_t row, size_t col);
 void	lex_tokenization(t_src *s, t_token *t);
 void	lex_endof(t_src *s, t_token *t);
 void	lex_comment(t_src *s, t_token *t);
+void	lex_header(t_src *s, t_token *t);
 
 #endif
