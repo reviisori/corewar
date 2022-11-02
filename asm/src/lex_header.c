c77bffce333d0e4572934ee98a934971429b7297
Aleksandr Tikka <altikka@student.hive.fi>
2022-11-02 18:02:46 +0200

    added function call to get ending quotation
    in this function we search for the trailing quotation and keep track of source's col & row

--- asm/src/lex_header.c
+++ asm/src/lex_header.c
@@ -6,22 +6,39 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 11:45:03 by atenhune          #+#    #+#             */
-/*   Updated: 2022/11/02 14:46:07 by atenhune         ###   ########.fr       */
+/*   Updated: 2022/11/02 17:37:23 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
+static char	*find_ending_quote(t_src *s)
+{
+	char	*ptr;
+
+	ptr = (char *)&s->buf.data[s->index + 1];
+	while (*ptr != '"')
+	{
+		s->col++;
+		if (*ptr == '\0')
+			return (NULL);
+		if (*ptr == '\n')
+		{
+			s->row++;
+			s->col = 0;
+		}
+		ptr++;
+	}
+	return (ptr);
+}
+
 static void	get_header_info(t_src *s, t_token *t)
 {
-	char *p;
+	char	*p;
 
 	if (*(char *)&s->buf.data[s->index] != '"')
-	{
-		ft_putendl("Error in .name or .comment (FIXTHIS)");
-		exit(0);
-	}
-	p = ft_strchr(&s->buf.data[s->index + 1], '"');
+		panic_lex(NULL, s->row, s->col);
+	p = find_ending_quote(s);
 	if (p)
 	{
 		ft_vecncat(&t->content, &s->buf.data[s->index],
@@ -41,6 +58,7 @@ void	lex_header(t_src *s, t_token *t)
 	{
 		t->symbol = la_champname;
 		s->index += 5;
+		s->col += 5;
 		skip_whitespace(s);
 		get_header_info(s, t);
 	}
@@ -48,6 +66,7 @@ void	lex_header(t_src *s, t_token *t)
 	{
 		t->symbol = la_champcomm;
 		s->index += 8;
+		s->col += 8;
 		skip_whitespace(s);
 		get_header_info(s, t);
 	}
