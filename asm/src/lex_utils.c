37ef5141416968e7fcf3ae1c932b83c9a9b9cdb5
atenhune <anttipekka.tenhunen@gmail.com>
2022-11-03 14:31:23 +0200

    is_label_char() added, panic_lex() now prints error message when champion name or comment is too long

--- asm/src/lex_utils.c
+++ asm/src/lex_utils.c
@@ -6,7 +6,7 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 12:02:35 by atenhune          #+#    #+#             */
-/*   Updated: 2022/11/02 17:59:53 by altikka          ###   ########.fr       */
+/*   Updated: 2022/11/03 14:27:52 by atenhune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,16 @@ void	panic_lex(const char *msg, size_t row, size_t col)
 {
 	if (!msg)
 		ft_dprintf(2, "Lexical error at [%zu:%zu]\n", row + 1, col + 1);
+	else if (ft_strequ(msg, "name") || ft_strequ(msg, "comment"))
+		ft_dprintf(2, "Champion %s too long (Max length %zu)\n", msg, row);
 	else
 		ft_dprintf(2, "call panic_syntax() maybe\n");
 	exit(EXIT_FAILURE);
+}
+
+int	is_label_char(int c)
+{
+	if ((c >= 97 && c <= 122) || c == 95 || (c >= 48 && c <= 57))
+		return (1);
+	return (0);
 }
