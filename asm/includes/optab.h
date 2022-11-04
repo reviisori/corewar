88994017b4e1e0bfad46e57f65b581443c254b8c
Aleksandr Tikka <altikka@student.hive.fi>
2022-11-04 14:41:27 +0200

    added struct for t_op and defined our op table

--- asm/includes/optab.h
+++ asm/includes/optab.h
@@ -6,7 +6,7 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 12:54:13 by atenhune          #+#    #+#             */
-/*   Updated: 2022/11/04 12:54:58 by atenhune         ###   ########.fr       */
+/*   Updated: 2022/11/04 14:40:43 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,31 @@
 
 typedef struct s_op
 {
-	char		*name; //maybe
 	u_int8_t	op_code;
-	int			args[3];
+	char		*name;
 	int			argc;
+	int			args[3];
 	bool		acb;
-	struct s_op	*next;
+	u_int8_t	size;
 }				t_op;
+
+static const t_op	g_optab[16] = {
+{1, "live", 1, {T_DIR}, false, 4},
+{2, "ld", 2, {T_DIR | T_IND, T_REG}, true, 4},
+{3, "st", 2, {T_REG, T_REG | T_IND}, true, 4},
+{4, "add", 3, {T_REG, T_REG, T_REG}, true, 4},
+{5, "sub", 3, {T_REG, T_REG, T_REG}, true, 4},
+{6, "and", 3, {T_REG | T_DIR | T_IND, T_REG | T_DIR | T_IND, T_REG}, true, 4},
+{7, "or", 3, {T_REG | T_DIR | T_IND, T_REG | T_DIR | T_IND, T_REG}, true, 4},
+{8, "xor", 3, T_REG | T_DIR | T_IND, T_REG | T_DIR | T_IND, T_REG, true, 4},
+{9, "jzmp", 1, {T_DIR}, false, 2},
+{10, "ldi", 3, {T_REG | T_DIR | T_IND, T_REG | T_DIR, T_REG}, true, 2},
+{11, "sti", 4, {T_REG, T_REG | T_DIR | T_IND, T_REG | T_DIR}, true, 2},
+{12, "fork", 1, {T_DIR}, false, 2},
+{13, "lld", 2, {T_DIR | T_IND, T_REG}, true, 4},
+{14, "lldi", 3, {T_REG | T_DIR | T_IND, T_REG | T_DIR, T_REG}, true, 2},
+{15, "lfork", 1, {T_DIR}, false, 2},
+{16, "aff", 1, {T_REG}, true, 4}
+};
 
 #endif
