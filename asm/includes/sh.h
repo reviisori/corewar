3701cdab01f9adc7bf4a395b1cc672488e1c314a
Aleksandr Tikka <altikka@student.hive.fi>
2022-11-08 17:11:43 +0200

    new struct for statements to later pass to assemble()

--- asm/includes/sh.h
+++ asm/includes/sh.h
@@ -6,16 +6,25 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 13:49:05 by altikka           #+#    #+#             */
-/*   Updated: 2022/11/04 12:55:05 by atenhune         ###   ########.fr       */
+/*   Updated: 2022/11/08 17:08:20 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SH_H
 # define SH_H
 
+typedef struct s_statement
+{
+	t_op		op;
+	u_int8_t	acb;
+	int			args[3];
+	u_int32_t	loc;
+}
+
 typedef struct s_sh
 {
 	t_header	header;
+	t_vec		footer;
 	t_hashmap	operations;
 }				t_sh;
 
