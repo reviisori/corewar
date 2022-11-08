a1557d8c262995fd57a968533f87d7334decd95a
Aleksandr Tikka <altikka@student.hive.fi>
2022-11-08 17:12:39 +0200

    better wip comments

--- asm/includes/symtypes.h
+++ asm/includes/symtypes.h
@@ -6,7 +6,7 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 17:07:22 by atenhune          #+#    #+#             */
-/*   Updated: 2022/10/28 18:53:56 by atenhune         ###   ########.fr       */
+/*   Updated: 2022/11/08 12:20:09 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@
 
 typedef enum e_symtypes
 {
-	la_unknown,
-	la_eof, //
-	la_eol, //
-	la_label, //
-	la_op, //
-	la_comm, // ??
-	la_champname, //
-	la_champcomm, //
-	la_reg, //
-	la_dir, //
-	la_ind, //
-	la_num,
-	la_comma, //??
-	la_minus,
-	la_plus
+	la_unknown,		//done
+	la_eof,			//done
+	la_eol,			//done 
+	la_label,		//needs 'add to hashmap'
+	la_op,			//done
+	la_comm,		//done
+	la_champname,	//done
+	la_champcomm,	//done
+	la_reg,			//
+	la_dir,			//
+	la_ind,			//
+	la_num,			//
+	la_comma,		//
+	la_minus,		//
+	la_plus			//good to have
 }	t_symtypes;
 
 typedef struct s_token
@@ -39,7 +39,7 @@ typedef struct s_token
 	bool			declared;
 	bool			is_label;
 	int				num;
-	struct t_token	*next;
+	struct t_token	*next; //not needed?
 }					t_token;
 
 #endif
