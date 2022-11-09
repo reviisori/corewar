4dcf167f04bd3e0a0bdb3fd8a814c98328a03705
Miro Apostolakis <mapostol@c2r1p2.hive.fi>
2022-11-09 14:17:28 +0200

    Added a getter for arguments

--- vm/sources/c_byte_functions.c
+++ vm/sources/c_byte_functions.c
@@ -22,6 +22,49 @@ unsigned char	get_crumb(unsigned char c_byte, int c_number)
 	return (crumb);
 }
 
+unsigned int	cast_arg_type_to_bytes(unsigned char arg_type, unsigned char op)
+{
+		if (arg_type == DIR_CODE)
+			return (g_op[op][OP_DIR]);
+		else if (arg_type == IND_CODE)
+			return (IND_SIZE);
+		else if (arg_type == REG_CODE)
+			return (1);
+		return (0);
+}
+
+static unsigned int	get_argument_wo_cbyte(t_info *info, unsigned char n, t_car *car)
+{
+	if (n != 1)
+		error_kill("invalid argument count ");//shouldn't ever ever ever happen
+	if(car->op == 0x01)
+		return ((info->memory[(car->pc + 1) % MEM_SIZE] << 24) 
+			+ (info->memory[(car->pc + 2) % MEM_SIZE] << 16)
+			+ (info->memory[(car->pc + 3) % MEM_SIZE] << 8)
+			+ (info->memory[(car->pc + 4) % MEM_SIZE]));
+	return ((info->memory[(car->pc + 1) % MEM_SIZE] << 8) 
+		+ (info->memory[(car->pc + 2) % MEM_SIZE]));
+}
+
+unsigned int	cat_n_bytes(unsigned char *offset, unsigned int bytes)
+{
+	int				i;
+	unsigned int	sum;
+	unsigned char 	*c;
+
+	i = 0;
+	sum = 0;
+	c = offset;
+	while (i < bytes)
+	{
+		sum = sum << 8;
+		sum += *c;
+		c++;
+		i++;
+	}
+	return (sum);
+}
+
 /* Finds argument n (where 1 <= n <= 3, 4 is for theoretical statements)
 	for the carriages current op */
 unsigned int	get_argument(t_info *info, unsigned char n, t_car *car)
@@ -33,27 +76,17 @@ unsigned int	get_argument(t_info *info, unsigned char n, t_car *car)
 	if (n < 1 || n > 4 || (n > 1 && !g_op[car->op][C_BYTE]))
 		error_kill("invalid argument count ");//shouldn't ever ever ever happen
 	if (!g_op[car->op][C_BYTE])
-	{
-		if(car->op == 0x01)
-			return ((info->memory[(car->pc + 1) % MEM_SIZE] << 6) 
-				+ (info->memory[(car->pc + 2) % MEM_SIZE] << 4)
-				+ (info->memory[(car->pc + 3) % MEM_SIZE] << 2)
-				+ (info->memory[(car->pc + 4) % MEM_SIZE]));
-		return ((info->memory[(car->pc + 1) % MEM_SIZE] << 2) 
-			+ (info->memory[(car->pc + 2) % MEM_SIZE]));
-	}
+		return (get_argument_wo_cbyte(info, n, car));
 	i = 1;
 	sum = 0;
 	while (i < n)
 	{
 		arg_type = get_crumb(info->memory[(car->pc + 1) % MEM_SIZE], i);
-		if (arg_type == DIR_CODE)
-			sum += g_op[car->op][OP_DIR];
-		else if (arg_type == IND_CODE)
-			sum += IND_SIZE;
-		else
-			sum += 1;
+		sum += cast_arg_type_to_bytes(arg_type, car->op);
 		i++;
 	}
 	arg_type = get_crumb(info->memory[(car->pc + 1) % MEM_SIZE], n);
+	i = cast_arg_type_to_bytes(arg_type, car->op);
+	return (cat_n_bytes(&info->memory[(car->pc + 2 + sum)
+		% MEM_SIZE], i));
 }
