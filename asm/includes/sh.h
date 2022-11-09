d26e8264c8e0aac5112946a8fff880daa11127c7
Aleksandr Tikka <altikka@student.hive.fi>
2022-11-09 17:40:16 +0200

    more varibles for structs to help on a long journey ahead

--- asm/includes/sh.h
+++ asm/includes/sh.h
@@ -6,7 +6,7 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 13:49:05 by altikka           #+#    #+#             */
-/*   Updated: 2022/11/08 17:08:20 by altikka          ###   ########.fr       */
+/*   Updated: 2022/11/09 17:18:28 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,18 @@
 typedef struct s_statement
 {
 	t_op		op;
-	u_int8_t	acb;
+	u_int8_t	acb; //
 	int			args[3];
+	int			cur_arg;
 	u_int32_t	loc;
-}
+}				t_statement;
 
 typedef struct s_sh
 {
 	t_header	header;
-	t_vec		footer;
-	t_hashmap	operations;
+	t_vec		code;
+	t_hashmap	ops;
+	u_int32_t	byte;
 }				t_sh;
 
 #endif
