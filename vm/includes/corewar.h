302e8f5a2ba8becbe5fc32f31a60b48b8d28cd5a
Arttu Suikkanen <arttu.suikkanen@hotmail.com>
2022-11-09 14:29:22 +0200

    Slight error in get_argument(?)

--- vm/includes/corewar.h
+++ vm/includes/corewar.h
@@ -187,6 +187,7 @@ void			usage_exit(void);
 void			error_kill(char *reason);
 
 /* VM tools */
+unsigned int	get_argument(t_info *info, unsigned char n, t_car *car);
 unsigned int	cast_arg_type_to_bytes(unsigned char arg_type, unsigned char op);
 
 /* Game functions */
