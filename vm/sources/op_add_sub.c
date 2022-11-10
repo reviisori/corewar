4a4f7291869de564538111e06e05ab93f19412cf
Arttu Suikkanen <arttu.suikkanen@hotmail.com>
2022-11-10 11:30:08 +0200

    Implemented sub

--- vm/sources/op_add_sub.c
+++ vm/sources/op_add_sub.c
@@ -36,6 +36,22 @@ void	op_add(t_info *info, t_car *car)
 
 void	op_sub(t_info *info, t_car *car)
 {
-	(void)info;
-	(void)car;
+	unsigned char	args[3];
+	unsigned int	sub;
+
+	ft_bzero(args, sizeof(args));
+	if (get_crumb(info->memory[(car->pc + 1) % MEM_SIZE], 1) != REG_CODE
+		|| get_crumb(info->memory[(car->pc + 1) % MEM_SIZE], 2) != REG_CODE
+		|| get_crumb(info->memory[(car->pc + 1) % MEM_SIZE], 3) != REG_CODE)
+		return ;
+	args[0] = get_argument(info, 1, car);
+	args[1] = get_argument(info, 2, car);
+	args[2] = get_argument(info, 3, car);
+	if ((args[0] > REG_NUMBER || !args[0])
+		|| (args[1] > REG_NUMBER || !args[1])
+		|| (args[2] > REG_NUMBER || !args[2]))
+		return ;
+	sub = car->reg[args[0]] - car->reg[args[1]];
+	car->reg[args[2]] = sub;
+	car->carry = 1;
 }
