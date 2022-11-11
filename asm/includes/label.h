08784aa965cb4e3ff8b9446c41308df2059a1b75
atenhune <anttipekka.tenhunen@gmail.com>
2022-11-11 15:42:19 +0200

    added variable to t_label and t_undeflab

--- asm/includes/label.h
+++ asm/includes/label.h
@@ -6,7 +6,7 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 12:12:18 by altikka           #+#    #+#             */
-/*   Updated: 2022/11/10 15:03:21 by atenhune         ###   ########.fr       */
+/*   Updated: 2022/11/11 14:52:42 by atenhune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,14 @@ typedef struct s_undeflab
 	u_int32_t			stmt;
 	u_int8_t			arg;
 	u_int32_t			loc;
+	bool				is_dir;
 	struct s_undeflab	*next;
 }						t_undeflab;
 
 typedef struct s_label
 {
 	bool				declared; //
+	bool				is_dir;
 	int					value; // 
 	struct s_undeflab	*head;
 }						t_label;
