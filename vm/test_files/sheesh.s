c389a40b283dd6ac3e25c91063193de6017a165b
Miro Apostolakis <mapostol@c2r1p2.hive.fi>
2022-11-11 14:50:22 +0200

    Fixed an issue with invalid coding bytes. Old version didn't calculate jump correctly

--- vm/test_files/sheesh.s
+++ vm/test_files/sheesh.s
@@ -1,8 +1,15 @@
 .name "Sheeeeeeeeeeesh"
 .comment "xD"
 
-ldi 1, %0, r3
+ldi %1, %0, r3
 st r3, 20
+ld %255, r2
+ld %1, r3
+st r1, 200
+add r1, r1, r2
+st r2, r4
+st r3, 20
+st r4, 30
 fork %25
 live %4294967295
 ld %255, r2
@@ -17,14 +24,6 @@ ld %1, r3
 live %4294967295
 fork %25
 live %4294967295
-ld %255, r2
-ld 1, r3
-st r1, 200
-add r1, r1, r2
-st r2, r4
-add r2, r2, r3
-st r3, 20
-st r4, 30
 ld %255, r2
 ld %1, r3
 live %4294967295
