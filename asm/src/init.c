0196d867c07bab88a16c0ffc0e566ea917894f3f
home <aleksandr.tikka@gmail.com>
2022-11-11 21:59:55 +0200

    added flag to init_hashmap() to diffirentiate ops from labels

--- asm/src/init.c
+++ asm/src/init.c
@@ -6,23 +6,26 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 12:12:50 by altikka           #+#    #+#             */
-/*   Updated: 2022/11/10 14:54:12 by atenhune         ###   ########.fr       */
+/*   Updated: 2022/11/11 20:18:51 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-static void	init_hashmap(t_hashmap *hm, size_t size)
+static void	init_hashmap(t_hashmap *hm, size_t size, bool is_optab)
 {
 	size_t	i;
 
 	if (hash_new(hm, size) < 0)
 		panic("Couldn't initialize hashmap.");
-	i = 0;
-	while (i < OPTAB_SIZE)
+	if (is_optab)
 	{
-		hash_insert(hm, g_optab[i].name, g_optab[i].op_code);
-		i++;
+		i = 0;
+		while (i < OPTAB_SIZE)
+		{	
+			hash_insert(hm, g_optab[i].name, g_optab[i].op_code);
+			i++;
+		}
 	}
 }
 
@@ -37,7 +40,7 @@ void	init_lex(t_token *t, t_labtab *lt)
 	t->num = 1;
 	t->next = NULL;
 	ft_bzero(lt, sizeof(*lt));
-	init_hashmap(&lt->labels, 100);
+	init_hashmap(&lt->labels, 100, false);
 	if (ft_vecnew(&lt->entries, 1, sizeof(t_labtab)) < 0)
 		panic("Couldn't initialize label table entries.");
 }
@@ -62,6 +65,6 @@ void	init_handler(t_sh *d)
 	d->header.prog_size = 0;
 	if (ft_vecnew(&d->code, 1, sizeof(t_statement)) < 0)
 		panic("Couldn't initialize exec code.");
-	init_hashmap(&d->ops, 32);
+	init_hashmap(&d->ops, 32, true);
 	d->byte = 0;
 }
