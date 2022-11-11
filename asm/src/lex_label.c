c5b016b26df7a27a5aee4d6b06a038eb486c2a21
atenhune <anttipekka.tenhunen@gmail.com>
2022-11-11 15:43:54 +0200

    get_label() now done

--- asm/src/lex_label.c
+++ asm/src/lex_label.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lex_label.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 15:39:02 by altikka           #+#    #+#             */
-/*   Updated: 2022/11/10 17:26:31 by altikka          ###   ########.fr       */
+/*   Updated: 2022/11/11 15:35:49 by atenhune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static void	label_add(t_sh *d, t_label *lab)
 {
-	t_statement *temp;
+	t_statement	*temp;
 	t_undeflab	*temp_node;
 	t_undeflab	*node;
 
@@ -39,29 +39,48 @@ static void	label_add(t_sh *d, t_label *lab)
 
 static char	*get_label(t_src *s, bool *is_arg)
 {
-	(void)s;
+	char	*key;
+	char	*p;
+
 	*is_arg = true;
-	return (NULL);
+	p = (char *)&s->buf.data[s->index];
+	if (*p == DIRECT_CHAR && *s->next == LABEL_CHAR)
+	{
+		p += 2;
+		source_adjust(s, 2);
+	}
+	else
+	{
+		p++;
+		source_adjust(s, 1);
+	}
+	while (is_label_chars(*p))
+		p++;
+	key = ft_strsub((char *)&s->buf.data[s->index], 0,
+			p - (char *)&s->buf.data[s->index]);
+	return (key);
 }	
 
 void	lex_label(t_sh *d, t_src *s, t_labtab *lt, char *key)
 {
 	t_label	lab;
-	int		value;
+	t_hash	*entry;
 	bool	is_arg;
 
 	is_arg = false;
 	if (!key)
 		key = get_label(s, &is_arg);
-	value = hash_get(&lt->labels, key);
-	if (value == -1 && is_arg == false)
+	entry = hash_get(&lt->labels, key);
+	if (!entry && is_arg == false)
 		hash_insert(&lt->labels, key, d->byte);
-	else if (value == -1 && is_arg == true)
+	else if (!entry && is_arg == true)
 	{
 		lab.declared = false;
 		lab.head = NULL;
 		label_add(d, &lab);
 		ft_vecpush(&lt->entries, &lab);
-		hash_insert(&lt->labels, key, ((int)lt->entries.len * -1) -1);
+		hash_insert(&lt->labels, key, ((int)lt->entries.len * -1) - 1);
 	}
+	if (is_arg == true)
+		ft_strdel(&key);
 }
