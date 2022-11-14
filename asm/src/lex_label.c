0c48533a2a27d2eea883aaa391d68d20b88295a3
Aleksandr Tikka <altikka@student.hive.fi>
2022-11-14 17:20:37 +0200

    day's work
    now in workable order what come's to labels and labels as args but needs major restructioning

--- asm/src/lex_label.c
+++ asm/src/lex_label.c
@@ -6,48 +6,87 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 15:39:02 by altikka           #+#    #+#             */
-/*   Updated: 2022/11/11 21:31:10 by altikka          ###   ########.fr       */
+/*   Updated: 2022/11/14 17:11:31 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-void	label_fill(t_sh *d, t_vec *entries, t_hash *entry)
+static void	label_set_arg(t_sh *d, t_hash *entry)
 {
-	//t_statement	*temp;
+	t_statement	*temp_stmt;
+
+	temp_stmt = ft_vecget(&d->code, d->code.len -1);
+	if (temp_stmt->is_dir)
+		temp_stmt->args[temp_stmt->cur_arg] = entry->value;
+	else
+		temp_stmt->args[temp_stmt->cur_arg] = d->byte - entry->value;
+}
+
+
+static void	label_fill(t_sh *d, t_vec *entries, t_hash *entry)
+{
+	t_statement	*temp_stmt;
+	t_undeflab	*temp_undeflab;
 	t_label		*label;
 
+	if (entry->value > -1)
+	{
+		label_set_arg(d, entry);
+		return ;
+	}
 	label = ft_vecget(entries, ft_abs(entry->value + 1));
-	ft_printf("....>%d\n", entry->value);
+	label->declared = true;
+	temp_undeflab = label->head;
+	while (temp_undeflab)
+	{
+		temp_stmt = ft_vecget(&d->code, temp_undeflab->stmt);
+		if (temp_undeflab->is_dir)
+			temp_stmt->args[temp_undeflab->arg] = d->byte;
+		else
+			temp_stmt->args[temp_undeflab->arg] = temp_undeflab->loc - d->byte;
+		temp_undeflab = temp_undeflab->next;
+	}
 	entry->value = d->byte;
-	ft_printf("....>%d\n", entry->value);
-	exit(1);
+	//remove from entries maybe
 }
 
 static void	label_add(t_sh *d, t_label *lab)
 {
-	t_statement	*temp;
+	t_statement	*temp_stmt;
 	t_undeflab	*temp_node;
 	t_undeflab	*node;
 
-	temp = ft_vecget(&d->code, d->code.len - 1);
+	temp_stmt = ft_vecget(&d->code, d->code.len - 1);
 	node = (t_undeflab *)ft_memalloc(sizeof(t_undeflab));
 	if (!node)
 		panic("Malloc error while adding a label node.");
 	node->stmt = d->code.len - 1;
-	node->arg = temp->cur_arg;
+	node->arg = temp_stmt->cur_arg;
 	node->loc = d->byte;
-	//node->is_dir = lab->is_dir;
+	node->is_dir = lab->is_dir;
 	node->next = NULL;
-	if (!lab->head)
-	{
-		lab->head = node;
-		return ;
-	}
-	temp_node = lab->head->next;
+	temp_node = lab->head;
 	while (temp_node->next)
 		temp_node = temp_node->next;
 	temp_node->next = node;
+}
+
+static void	label_new(t_sh *d, t_label *lab)
+{
+	t_statement	*temp_stmt;
+	t_undeflab	*node;
+
+	temp_stmt = ft_vecget(&d->code, d->code.len - 1);
+	node = (t_undeflab *)ft_memalloc(sizeof(t_undeflab));
+	if (!node)
+		panic("Malloc error while adding a label node.");
+	node->stmt = d->code.len - 1;
+	node->arg = temp_stmt->cur_arg;
+	node->loc = d->byte;
+	node->is_dir = lab->is_dir;
+	node->next = NULL;
+	lab->head = node;
 }
 
 static char	*get_label(t_src *s, bool *is_arg)
@@ -73,7 +112,18 @@ static char	*get_label(t_src *s, bool *is_arg)
 			p - (char *)&s->buf.data[s->index]);
 	source_adjust(s, ft_strlen(key));
 	return (key);
-}	
+}
+
+static void	label_init(t_sh *d, t_label *l, bool declared, bool is_first)
+{
+	t_statement	*stmt;
+
+	stmt = ft_vecget(&d->code, d->code.len - 1);
+	l->declared = declared;
+	l->is_dir = stmt->is_dir;
+	if (is_first)
+		l->head = NULL;
+}
 
 void	lex_label(t_sh *d, t_src *s, t_labtab *lt, char *key)
 {
@@ -90,17 +140,22 @@ void	lex_label(t_sh *d, t_src *s, t_labtab *lt, char *key)
 		hash_insert(&lt->labels, key, d->byte);
 	else if (!entry && is_arg == true)
 	{
-		label.declared = false;
-		//label.is_dir = REMEMBER ME
-		label.head = NULL;
-		label_add(d, &label);
+		label_init(d, &label, false, true);
+		label_new(d, &label);
 		ft_vecpush(&lt->entries, &label);
 		hash_insert(&lt->labels, key, ((int)lt->entries.len * -1));
 	}
 	else if (entry && is_arg == true)
-		ft_printf("you need me!\n");
+	{
+		if (entry->value < 0)
+		{
+			label_init(d, ft_vecget(&lt->entries, ft_abs(entry->value + 1)),
+				false, false);
+			label_add(d, ft_vecget(&lt->entries, ft_abs(entry->value + 1)));
+		}
+		else
+			label_fill(d, &lt->entries, entry);
+	}
 	else if (entry && is_arg == false)
 		label_fill(d, &lt->entries, entry);
-	//if (is_arg == true)
-	//	ft_strdel(&key);
 }
