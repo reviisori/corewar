3704143521fb919c26c906910119e624ee040a29
Arttu Suikkanen <arttu.suikkanen@hotmail.com>
2022-11-17 14:51:32 +0200

    Fixed some bugs in print functions

--- vm/sources/print_st_sti.c
+++ vm/sources/print_st_sti.c
@@ -21,14 +21,14 @@ void	print_st(t_car *car, unsigned int args[])
 void	print_sti(t_car *car, unsigned int args[])
 {
 	int				sum;
-	unsigned short	target_adr;
+	short	target_adr;
 
 	ft_printf("P%5d | ", car->index);
 	ft_printf("sti r%hhd ", args[0]);
 	ft_printf("%d ", args[1]);
 	ft_printf("%d\n", args[2]);
 	sum = args[1] + args[2];
-	target_adr = (car->pc + (short)(sum % IDX_MOD)) % MEM_SIZE;
+	target_adr = (car->pc + ((short)sum % IDX_MOD)) % MEM_SIZE;
 	ft_printf("%7c| -> store to %d + %d = %d (with pc and mod %hu)\n",
 		' ',
 		args[1],
