10767330ecaa24b558c1608f2e5438a14f44f93b
Arttu Suikkanen <arttu.suikkanen@hotmail.com>
2022-11-21 16:56:18 +0200

    Finally fixed sti (fingers crossed)

--- vm/sources/print_st_sti.c
+++ vm/sources/print_st_sti.c
@@ -20,30 +20,15 @@ void	print_st(t_car *car, unsigned int args[])
 	ft_printf("st r%hhd %hd\n", args[0], args[1]);
 }
 
-void	print_sti(t_car *car, unsigned int args[], unsigned char arg_types[])
+void	print_sti(t_car *car, int args[])
 {
 	int		sum;
-	int		args_final[3];
 	short	target_adr;
 
-<<<<<<< HEAD
-	ft_memcpy(args_final, args, sizeof(unsigned int) * 3);
-	if (arg_types[1] == DIR_CODE && (short)args_final[1] < 0)
-		args_final[1] = args_final[1] | RIGHTMOST_TWO_BYTES;
-	if (arg_types[2] == DIR_CODE && (short)args_final[2] < 0)
-		args_final[2] = args_final[2] | RIGHTMOST_TWO_BYTES;
-	sum = args_final[1] + args_final[2];
+	sum = args[1] + args[2];
 	target_adr = car->pc + sum % IDX_MOD;
 	print_process_id(car->index);
-	ft_printf("sti r%hhd %d %d\n", args_final[0], args_final[1], args_final[2]);
+	ft_printf("sti r%hhd %d %d\n", args[0], args[1], args[2]);
 	ft_printf("%7c| -> store to %d + %d = %d (with pc and mod %hd)\n",
-		' ', args_final[1], args_final[2], sum, target_adr);
-=======
-	sum = args[1] + args[2];
-	target_adr = (car->pc + ((short)sum % IDX_MOD));
-	ft_printf("P%5d | ", car->index);//vittumaisesti prosessi voi olla yli neljä numeroa == error
-	ft_printf("sti r%hhd %hd %hd\n", args[0], args[1], args[2]);
-	ft_printf("%7c| -> store to %hd + %hd = %hd (with pc and mod %hd)\n",
 		' ', args[1], args[2], sum, target_adr);
->>>>>>> vm_champions
 }
