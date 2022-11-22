cc3870e3238b23e1f509a26bb9a6535a401a5a69
atenhune <anttipekka.tenhunen@gmail.com>
2022-11-22 16:25:55 +0200

    variables added to structs

--- asm/includes/sh.h
+++ asm/includes/sh.h
@@ -6,7 +6,7 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 13:49:05 by altikka           #+#    #+#             */
-/*   Updated: 2022/11/15 15:30:50 by atenhune         ###   ########.fr       */
+/*   Updated: 2022/11/22 12:57:03 by atenhune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ typedef struct s_statement
 	t_op		op;
 	u_int8_t	acb;			//
 	int			args[3];
+	u_int8_t	arg_type[3];
 	int			cur_arg;
 	int			sep;
 	bool		is_dir;
@@ -29,6 +30,7 @@ typedef struct s_sh
 	t_header	header;
 	t_vec		code;
 	t_hashmap	ops;
+	char		*filename;
 	u_int32_t	byte;
 }				t_sh;
 
