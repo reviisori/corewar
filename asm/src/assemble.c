dba0828a2123a0b1ea70b01d648b3c2bb4087fdc
atenhune <anttipekka.tenhunen@gmail.com>
2022-11-22 16:26:43 +0200

    call to write_file() added

--- asm/src/assemble.c
+++ asm/src/assemble.c
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   assemble.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 15:17:29 by altikka           #+#    #+#             */
-/*   Updated: 2022/10/26 15:19:46 by altikka          ###   ########.fr       */
+/*   Updated: 2022/11/22 16:08:46 by atenhune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-int	assemble(char *filename)
+int	assemble(t_sh *d, char *filename)
 {
-	(void)filename;
+	write_file(d, filename);
 	ft_printf("asm done - I wish.\n");
 	return (1);
 }
