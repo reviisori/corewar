5c5fe8ac41e8c6dcb54dfdf22027708d5be02b56
atenhune <anttipekka.tenhunen@gmail.com>
2022-12-10 13:20:18 +0200

    added few things to test

--- assembler/tests_asm/valid_files/moro.s
+++ assembler/tests_asm/valid_files/moro.s
@@ -6,3 +6,8 @@ hihii1:
 hihii2:
 
 		live %:hihii1
+hihii:
+		ld %12, r2
+		live %:hihii
+		ld 12, r19
+
--- assembler/tests_asm/valid_files/zork.s
+++ assembler/tests_asm/valid_files/zork.s
@@ -2,7 +2,7 @@
 .comment "I'M ALIIIIVE"
 
 l2:		sti r1, %:live, %1
-		and r1, %0, r1
+		and r1, %0, r16
 
-live:	live %1
+live:	live %12098345729385728934572938547298345729083457209835472098345729834570
 		zjmp %:live
